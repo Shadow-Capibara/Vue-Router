3906761ff36f8f315e8280841c45e8d0aaca8708
yosakiva <138710107+yosakiva@users.noreply.github.com>
2024-07-19 00:55:23 +0700

    Move folder

---
+++ 331-intro-to-vite-master/src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '@/views/EventListView.vue'
+import AboutView from '@/views/AboutView.vue'
+import StudentView from '@/views/StudentView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list-view',
+      component: EventListView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/Student',
+      name: 'Student',
+      component: StudentView
+    }
+  ]
+})
+
+export default router
