58e07b21b1b7fa3a93434483737d8f63efa7fbaa
yosakiva <138710107+yosakiva@users.noreply.github.com>
2024-07-19 10:29:41 +0700

    1.7

--- 331-intro-to-vite-master/src/components/EventCard.vue
+++ 331-intro-to-vite-master/src/components/EventCard.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import Event from '@/types/Event'
 import { RouterLink } from 'vue-router';
 defineProps<{
@@ -19,7 +18,7 @@ event: Event
 </script>
 
 <template>
-  <RouterLink to="/event/5928101">
+  <RouterLink :to="{name: 'event-detail-view', params: {id:event.id} }">
     <div class="event-card">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
