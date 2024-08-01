e5c8591fabadf27f29c815c47d3a2a39d2d3d204
yosakiva <138710107+yosakiva@users.noreply.github.com>
2024-08-01 15:33:32 +0700

    2.9

--- 331-intro-to-vite-master/src/views/EventListView.vue
+++ 331-intro-to-vite-master/src/views/EventListView.vue
@@ -41,12 +41,25 @@ import { error } from 'console'
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
     <EventDetails v-for="event in events" :key="event.id" :event="event"/>
+    <div class="pagination">
+    <RouterLink 
+    id="page-prev"
+    :to="{name: 'event-list-view', query: { page:page-1}}"
+    rel="prev"
+    v-if="page != 1"
+    >&%60; Prev Page
+    </RouterLink>
+    <RouterLink
+    id="page-next"
+    :to="{name: 'event-list-view', query:{page: page + 1}}"
+    rel="next"
+    v-if="hasNextPage"
+    >Next Page &#62;
+    </RouterLink>
   </div>
-  <RouterLink 
-  :to="{name: 'event-list-view', query:{ page: page + 1}}" 
-  rel="next" 
-  v-if="hasNextPage"
-  >Next page</RouterLink>
+  </div>
+  
+ 
 </template>
 
 <style scoped>
@@ -54,5 +67,20 @@ import { error } from 'console'
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination{
+  display: flex;
+  width: 290px;
+}
+.pagination a{
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev{
+  text-align: left;
+}
+#page-next{
+  text-align: right;
 }
 </style>
