3b209b425fa030c95c060a8c57aa5cf3a67d74cf
yosakiva <138710107+yosakiva@users.noreply.github.com>
2024-08-06 21:53:07 +0700

    4.1

--- 331-intro-to-vite-master/src/views/event/RegisterView.vue
+++ 331-intro-to-vite-master/src/views/event/RegisterView.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-import { toRefs } from 'vue';
+import { toRefs } from 'vue'
 import { type Event } from '@/types'
 
 const props = defineProps<{
-  event: Event
-  id: String
+    event: Event
+    id: String
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { event } = toRefs(props)
+const { event } = toRefs (props)
+const register = () => {
+    // if the registration API call successful
+    // Push back to the event details view
+}
 </script>
-
 <template>
   <p>Register event here</p>
+  <button @click="register">Register</button>
 </template>
